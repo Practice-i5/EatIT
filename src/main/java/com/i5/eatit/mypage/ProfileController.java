bc82f58e54a96fa83cbce37ab248cee6cbad833f
gsh118 <hks01108@naver.com>
2024-07-13 19:32:10 +0900

    이미지 타임리프 url 수정, 타임리프 프로필 불러오는 테스트 url 추가

--- src/main/java/com/i5/eatit/mypage/ProfileController.java
+++ src/main/java/com/i5/eatit/mypage/ProfileController.java
@@ -1,7 +1,9 @@
 package com.i5.eatit.mypage;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/my-page/*")
@@ -10,4 +12,15 @@ public class ProfileController {
 
     @GetMapping("profile")
     public void profile() {}
+
+    @GetMapping("profile/test")
+    public String profileTest(Model model){
+        model.addAttribute("profileImage", "/img/my-page/파르토.webp");
+        model.addAttribute("nickName", "팡요");
+        model.addAttribute("gender", "male");
+        model.addAttribute("age", 27);
+        model.addAttribute("introduce", "판교에 서식하는 자바 백엔드 개발자, 디저트 카페 찾는걸 좋아함.");
+
+        return "my-page/profile";
+    }
 }
