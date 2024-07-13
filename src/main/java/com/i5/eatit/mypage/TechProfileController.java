236c15d6e44b7a28deb5b476e22649c10cf03b43
gsh118 <hks01108@naver.com>
2024-07-13 21:21:31 +0900

    기술 스택 체크용 코드, 타임리프 추가

--- src/main/java/com/i5/eatit/mypage/TechProfileController.java
+++ src/main/java/com/i5/eatit/mypage/TechProfileController.java
@@ -1,8 +1,14 @@
 package com.i5.eatit.mypage;
 
+import com.i5.eatit.common.DTO.techStackDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/my-page/*")
@@ -10,4 +16,18 @@ public class TechProfileController {
 
     @GetMapping("tech-profile")
     public void techProfile() {}
+
+    @GetMapping("tech-profile/test")
+    public String getTechStack(Model model) {
+        List<techStackDTO> techStackList = new ArrayList<>();
+        techStackList.add(new techStackDTO(1, "백엔드", "backend"));
+        techStackList.add(new techStackDTO(2, "프론트엔드", "frontend"));
+        techStackList.add(new techStackDTO(3, "게임 클라이언트", "game client"));
+        techStackList.add(new techStackDTO(4, "기획", "project management"));
+        techStackList.add(new techStackDTO(5, "인공지능", "ai"));
+
+        model.addAttribute("techStackList", techStackList);
+
+        return "/my-page/tech-profile";
+    }
 }
