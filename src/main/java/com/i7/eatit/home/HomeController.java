d5cec1e4dbc288839f69de7645ae3cf15bcd47de
☀️🚀🍀✈️🐨 <gpwls7357@naver.com>
2024-07-18 18:24:46 +0900

    initial commit & home 파일 구조 완성!

---
+++ src/main/java/com/i7/eatit/home/HomeController.java
@@ -0,0 +1,12 @@
+package com.i7.eatit.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HomeController {
+
+
+}
