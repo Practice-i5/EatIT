79c5bf9b6326a83fc5e13a89edcc936696744956
MungChi <2ee42a45@gmail.com>
2024-07-18 15:38:34 +0900

    review thymeleaf 중

--- src/main/java/com/i5/eatit/ReviewController.java
+++ src/main/java/com/i5/eatit/ReviewController.java
@@ -2,17 +2,43 @@ package com.i5.eatit;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/review/*")
+@RequestMapping
 public class ReviewController {
 
-    @GetMapping("/memberList")
-    public String goReview() {
-        return "review/memberList";
+    /*
+    페이지 이동
+     */
+    
+    // 리뷰하러 가는 페이지
+    @GetMapping("/review/reviewMain")
+    public void goReview() {
     }
 
-    // 모임 정보 받아오기(시간, 모임제목, 참가자(호스트, 게스트)
+    // 상세 페이지로 이동
+    @GetMapping("/index")
+    public void goIndex() {
+    }
 
+    /*
+    모임 정보 가져오기 (시간, 모임제목, 참가자(호스트, 게스트)
+     */
+
+    @GetMapping()
+
+
+    /*
+    리뷰 등록하기 - 체크한 체크박스를 db에 저장
+     */
+    
+    
+    
+    // 모임 정보 받아오기(시간, 모임제목, 참가자(호스트, 게스트)
+    @PostMapping("/review/regist")
+    public void goRegister() {
+        
+    }
 }
