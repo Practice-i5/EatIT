6c527e0b0cae0517bfad6fda11348df49313c9f9
Virtus_Chae <ybd4731@gmail.com>
2024-07-22 17:19:41 +0900

    feat: 실제 DB 에서 값을 가져오도록 로직 수정

---
+++ src/main/java/com/i7/eatit/admin/mapper/AdminMapper.java
@@ -0,0 +1,11 @@
+package com.i7.eatit.admin.mapper;
+
+import com.i7.eatit.admin.dto.AdminMemberDto;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AdminMapper {
+
+    List<AdminMemberDto> findAllMember();
+}
