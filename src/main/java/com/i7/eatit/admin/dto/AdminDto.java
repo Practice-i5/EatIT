3a22b17388659dcd9fc590d6de9fb7722395f689
Virtus_Chae <ybd4731@gmail.com>
2024-07-23 18:14:00 +0900

    feat: 로그인 관련 로직 추가
    - 상세한 부분은 내일 중으로 구현 예정입니다.
    - Filter, SessionID 등으로 구성해 보겠습니다.

---
+++ src/main/java/com/i7/eatit/admin/dto/AdminDto.java
@@ -0,0 +1,53 @@
+package com.i7.eatit.admin.dto;
+
+import java.time.LocalDateTime;
+
+public class AdminDto {
+
+    int adminId;
+    String email;
+    String password;
+    LocalDateTime createdDate;
+
+    public AdminDto(int adminId, String email, String password, LocalDateTime createdDate) {
+        this.adminId = adminId;
+        this.email = email;
+        this.password = password;
+        this.createdDate = createdDate;
+    }
+
+    public AdminDto() {
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+}
--- src/main/java/com/i7/eatit/admin/mapper/AdminMapper.java
+++ src/main/java/com/i7/eatit/admin/mapper/AdminMapper.java
@@ -1,11 +1,10 @@
 package com.i7.eatit.admin.mapper;
 
-import com.i7.eatit.admin.dto.AdminMemberDto;
-import java.util.List;
+import com.i7.eatit.admin.dto.AdminDto;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface AdminMapper {
 
-    List<AdminMemberDto> findAllMember(String sort, String searchEmail);
+    AdminDto findByEmail(String email);
 }
