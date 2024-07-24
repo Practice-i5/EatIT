5ee95b5e1ff25b54b822b4232acd031d28516484
YachaTree <dnftksghkfkd@icloud.com>
2024-07-24 16:53:58 +0900

    feat: 그룹채팅 소켓 적용중

--- src/main/resources/static/js/chat/chat.js
+++ src/main/resources/static/js/chat/chat.js
@@ -1,3 +1,23 @@
+const chatNamespace = io("/chat", {
+    auth: {
+        token: 123456,
+    },
+});
+
+
+// Query DOM
+const messageInput = document.getElementById("messageInput");
+const chatForm = document.getElementById("chatForm");
+const chatBox = document.getElementById("chat-box");
+const feedback = document.getElementById("feedback");
+const onlineUsers = document.getElementById("online-users-list");
+const chatContainer = document.getElementById("chatContainer");
+
+
+
+
+
+
 // start: Sidebar
 /**
  * 왼쪽 하단 클릭하면
@@ -77,33 +97,90 @@ document.querySelectorAll('.conversation-back').forEach(function(item) {
 });
 // end: Conversation
 
-document.addEventListener('DOMContentLoaded', function() {
-    // 초기 상태에서 1대1 채팅을 보이도록 설정
-    document.querySelector('#1on1chat').style.display = 'block';
-    document.querySelector('#groupChat').style.display = 'none';
-});
 
 /**
  * 클릭시 1대1채팅, 단톡방 오픈
  */
-document.querySelectorAll('.chat-toggle').forEach(function(item) {
-    item.addEventListener('click', function(e) {
-        e.preventDefault();
-        const target = this.dataset.target;
+// document.querySelectorAll('.chat-toggle').forEach(function(item) {
+//     item.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         const target = this.dataset.target;
+//
+//         // 모든 chat-content를 숨김
+//         document.querySelectorAll('.chat-content').forEach(function(content) {
+//             content.style.display = 'none';
+//         });
+//
+//         // 선택된 chat-content를 표시
+//         const targetElement = document.querySelector(target);
+//         if (targetElement) {
+//             targetElement.style.display = 'block';
+//         }
+//
+//         // 1대1 채팅 버튼을 클릭했을 때 초기 상태로 설정
+//
+//     });
+// });
 
-        // 모든 chat-content를 숨김
-        document.querySelectorAll('.chat-content').forEach(function(content) {
-            content.style.display = 'none';
-        });
+// *******************************************************서버로부터의 이벤트 리스닝
 
-        // 선택된 chat-content를 표시
-        const targetElement = document.querySelector(target);
-        if (targetElement) {
-            targetElement.style.display = 'block';
-        }
-
-        // 1대1 채팅 버튼을 클릭했을 때 초기 상태로 설정
-
-    });
+chatNamespace.on("chat message", (data) => {
+    feedback.innerHTML = "";
+    chatBox.innerHTML += `<li class="conversation-item-wrapper">
+                      <div class="conversation-item-box">
+                        <div class="conversation-item-text">
+                          <p>
+                            ${data.message}  <!--보내는 메세지 -->
+                          </p>
+                          <div class="conversation-item-time">${data.date}</div><!--보내는 시간 -->
+                        </div>
+                        <div class="conversation-item-dropdown">
+                          <button
+                                  type="button"
+                                  class="conversation-item-dropdown-toggle"
+                          >
+                            <i class="ri-more-2-line"></i>
+                          </button>
+                          <ul class="conversation-item-dropdown-list">
+                            <li>
+                              <a href="#"
+                              ><i class="ri-share-forward-line"></i>
+                                Forward</a
+                              >
+                            </li>
+                            <li>
+                              <a href="#"
+                              ><i class="ri-delete-bin-line"></i> Delete</a
+                              >
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </li>`;
+    chatContainer.scrollTop =
+        chatContainer.scrollHeight - chatContainer.clientHeight;
 });
 
+//*******************************************************타이핑 이벤트
+messageInput.addEventListener("keypress", (e) => {
+    chatNamespace.emit("typing", { name: nickname, roomNumber });
+});
+
+chatNamespace.on("typing", (data) => {
+    if (roomNumber == data.roomNumber) {
+        feedback.innerHTML = data;
+    }
+});
+
+//*******************************************************공용 채팅 메시지 전송 이벤트
+chatForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (messageInput.value) {
+        chatNamespace.emit("chat message", {
+            message: messageInput.value,
+            nickname,
+            roomNumber,
+        });
+        messageInput.value = "";
+    }
+});
