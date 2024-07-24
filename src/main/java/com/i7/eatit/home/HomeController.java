4ce2ec2d0700a55dcafb275c2e82039c7e993f38
dev-hjk <gpwls7357@naver.com>
2024-07-24 14:14:19 +0900

    feat: [BE]home.controller 추가 및 home.html 알람, 로그인, 회원가입 버튼 수정

--- src/main/java/com/i7/eatit/home/HomeController.java
+++ src/main/java/com/i7/eatit/home/HomeController.java
@@ -5,10 +5,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home/*")
+@RequestMapping
 public class HomeController {
 
-    @GetMapping("home")
-    public void home() {}
+    @GetMapping("/")
+    public String home() {
+        return "home/home"; // 이것은 templates/home 디렉토리의 home.html을 가리킵니다
+    }
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "home/home"; // 홈 페이지
+    }
+
+    @GetMapping("/enroll-meeting")
+    public String enrollMeeting() {
+        return "enroll-meeting/enroll-meeting"; // 모임 등록 페이지
+    }
+
+    @GetMapping("/search")
+    public String search() {
+        return "search/list"; // 모임 찾기 페이지
+    }
+
+    @GetMapping("/meetingList")
+    public String beforeMeetingList() {
+        return "meetingList/participatingList"; // 모임 목록 페이지
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login/loginMain"; // 로그인 페이지
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "login/join"; // 회원가입 페이지
+    }
+
+    @GetMapping("/alarm")
+    public String alarm() {
+        return "alarm/alarm"; // 알람 페이지
+    }
 
 }
