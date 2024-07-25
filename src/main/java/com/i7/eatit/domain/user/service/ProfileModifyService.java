e376a886d57e1e7fed7eb9df329a72bf564a5664
gsh118 <hks01108@naver.com>
2024-07-25 14:27:23 +0900

    비밀번호 변경 기능 추가

--- src/main/java/com/i7/eatit/domain/user/service/ProfileModifyService.java
+++ src/main/java/com/i7/eatit/domain/user/service/ProfileModifyService.java
@@ -5,6 +5,9 @@ import com.i7.eatit.domain.user.dto.ProfileModifyDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class ProfileModifyService {
 
@@ -18,6 +21,19 @@ public class ProfileModifyService {
     public void modifyProfile(ProfileModifyDTO profile){
         profileModifyMapper.updateProfile(profile);
 
+    }
+
+    @Transactional
+    public void modifyPassword(int memberId, String newPassword){
+        Map<String, String> info = new HashMap<>();
+        info.put("memberId", memberId+"");
+        info.put("newPassword", newPassword);
+        profileModifyMapper.updatePassword(info);
+    }
+
+    @Transactional
+    public void withdraw(int memberId){
+        profileModifyMapper.softDeleteMember(memberId);
     }
 
 
