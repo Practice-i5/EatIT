bac551ccaa7ec430369ff33f7d494b519e85852e
MungChi <2ee42a45@gmail.com>
2024-07-25 15:14:31 +0900

    meetingList 수정
    meetingDTO -> PreviewMeetingDTO

--- src/main/java/com/i7/eatit/domain/meetingList/controller/MeetingListController.java
+++ src/main/java/com/i7/eatit/domain/meetingList/controller/MeetingListController.java
@@ -2,12 +2,15 @@ package com.i7.eatit.domain.meetingList.controller;
 
 
 import com.i7.eatit.domain.meeting.model.dto.MeetingDTO;
+import com.i7.eatit.domain.meeting.model.dto.PreviewMeetingDTO;
 import com.i7.eatit.domain.meeting.model.service.MeetingService;
 
+import com.i7.eatit.domain.user.dto.UserInfoDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
 
@@ -21,32 +24,25 @@ public class MeetingListController {
         this.meetingService = meetingService;
     }
 
-    // 찜 목록
-    @GetMapping("/meetingList/likeList")
-    public String allLikeMeeting(Model model) {
 
-        model.addAttribute("meetingList", meetingService.findAllMeetings());
-        return "meetingList/likeList";
-    }
-
-
-    @GetMapping("/meetingList/followList")
-    public String allFollowMeeting(Model model) {
-
-        model.addAttribute("meetingList", meetingService.findAllMeetings());
-        return "meetingList/followList";
-    }
-
+    // 진행중인 모임 목록
     @GetMapping("/meetingList/participatingList")
-    public String allParticipatingMeeting(Model model) {
+    public String ParticipatingMeeting(@SessionAttribute("loginUser") UserInfoDTO userInfoDTO, Model model) {
 
-        model.addAttribute("meetingList", meetingService.findAllMeetings());
+        List<PreviewMeetingDTO> meetingList = meetingService.findPreviewByMemberIdOpen(userInfoDTO.getMember_id());
+
+        model.addAttribute("meetingList", meetingList);
+
         return "meetingList/participatingList";
     }
 
+
+    // 종료한 모임 목록
     @GetMapping("/meetingList/endList")
-    public String allEndMeeting(Model model) {
-        List<MeetingDTO> meetingList = meetingService.findAllMeetings();
+    public String EndMeeting(@SessionAttribute("loginUser") UserInfoDTO userInfoDTO, Model model) {
+
+        List<PreviewMeetingDTO> meetingList = meetingService.findPreviewByMemberIdOpen(userInfoDTO.getMember_id());
+
         model.addAttribute("meetingList", meetingList);
 
         System.out.println(meetingList);
@@ -54,10 +50,33 @@ public class MeetingListController {
         return "meetingList/endList";  // HTML 파일의 이름(확장자 제외)
     }
 
+
+
+
+
+    // 팔로잉한 모임 목록
+    @GetMapping("/meetingList/followList")
+    public String FollowMeeting(Model model) {
+
+        model.addAttribute("meetingList", meetingService.findAllMeetings());
+        return "meetingList/followList";
+    }
+
+
+    // 찜한 모임 목록
+    @GetMapping("/meetingList/likeList")
+    public String LikeMeetingList(Model model) {
+
+        model.addAttribute("meetingList", meetingService.findAllMeetings());
+        return "meetingList/likeList";
+    }
+
+
     @GetMapping("/meetingList/like2")
-    public String allLike2Meeting(Model model) {
+    public String Like2Meeting(Model model) {
         List<MeetingDTO> meetingList = meetingService.findAllMeetings();
         model.addAttribute("meetingList", meetingList);
         return "meetingList/likeList2";  // HTML 파일의 이름(확장자 제외)
     }
+
 }
