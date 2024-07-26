53216bb96df6ba2447197a6829a955f72f90e6f3
MungChi <2ee42a45@gmail.com>
2024-07-26 16:05:18 +0900

    joinUserProfile 생성

--- src/main/java/com/i7/eatit/domain/user/dao/JoinMemberProfileMapper.java
+++ src/main/java/com/i7/eatit/domain/user/dao/JoinMemberProfileMapper.java
@@ -1,8 +1,11 @@
 package com.i7.eatit.domain.user.dao;
 
 import com.i7.eatit.domain.user.dto.JoinMemberProfileDTO;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+
+@Mapper
 
 public interface JoinMemberProfileMapper {
 
